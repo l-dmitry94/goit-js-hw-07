ad896d85805642790ab84a5aabb700e6d942f552
Dmytro <lyubadmitry2@gmail.com>
2023-09-09 12:58:07 +0300

    feature/compete-2-task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,50 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+let instance = '';
+
+function createGalleryItems() {
+    const items = [];
+
+    galleryItems.forEach(galleryItem => {
+        const item = `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${galleryItem.original}">
+                <img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}">
+            </a>
+        </li>
+        `;
+        items.push(item);
+    })
+
+    const newItems = items.join("");
+    galleryList.insertAdjacentHTML("beforeend", newItems);
+}
+
+createGalleryItems();
+
+galleryList.addEventListener("click", selectImg);
+
+function selectImg(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const selectedImg = event.target.dataset.source;
+
+    instance = basicLightbox.create(`
+        <img src="${selectedImg}" width="800" height="600">`);
+    instance.show()
+}
+
+document.addEventListener('keydown', keyPress);
+
+function keyPress(event) {
+    if (event.key === "Escape" && instance != '') {
+        instance.close();
+        instance = '';
+    }
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+createGalleryItems();
+
+function createGalleryItems() {
+    const items = [];
+
+    galleryItems.forEach(galleryItem => {
+        const item = `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${galleryItem.original}">
+                <img class="gallery__image" src="${galleryItem.preview}" alt="${galleryItem.description}">
+            </a>
+        </li>
+        `;
+        items.push(item);
+    })
+
+    const newItems = items.join("");
+    galleryList.insertAdjacentHTML("beforeend", newItems);
+}
+
+let galleryItem = new SimpleLightbox('.gallery__link', {
+    captions: true,
+    captionsType: 'alt',
+    captionDelay: 250,
+    captionPosition: 'bottom'
+});
